7a7551d4d51e1e1c808f4dd8f19c91d2f69603b6
MarianaMoal <mariana.moreno@idesie.com>
2025-02-26 17:30:36 +0100

    Update 1.1_Selects-simples_GrupoX.sql

--- 1 Select simples/Alegra/1.1_Selects-simples_GrupoX.sql
+++ 1 Select simples/Alegra/1.1_Selects-simples_GrupoX.sql
@@ -3,53 +3,89 @@
 ------------------------------------------------------------------------------------------------
 /* 1
 Describir la tabla floors
-*/
+*/ 
+desc floors;
 
 /* 2
 Describir la tabla spaces
 */
+desc spaces;
 
 /* 3
 Datos de la tabla components
-*/
+el asterisco quiere decir que selecciona "todo"*/
+select *
+from components;
 
 /* 4
 Datos de la tabla component_types
 */
+select *
+from component_types;
 
 /* 5
 Id, nombre de los facilities
 */
+select id,name
+from facilities;
 
 /* 6
 Nombre, elevación e id del facility de las plantas
 */
+select name,elevation,facilityid
+from floors;
 
 /* 7
 Nombre, area bruta, volumen de los espacios
 */
+select name,grossarea,volume
+from spaces;
 
 /* 8
 Nombre, vida útil de los tipos de componentes del facility 1
 */
+select name,EXPECTEDLIFE,facilityid
+from component_types
+where facilityid=1;
 
 /* 9
 Nombre de los espacios de la Planta 1 del facility 1
 */
 /*Previamente se consulta cuál es el floorid
-listando los */
+listandolos... en where facilityid=1 and name='Planta +1' pude haber puesto where facilityid=1 and name like='%Planta%'... 
+esto me muestra todo lo que contenga la palabra planta y así verifico que numero es mi planta que estoy buscando
+también podría usar '_l%' esto significa que busco algo que en el segundo caracter tengo una l*/
+select id,name
+from floors
+where facilityid=1 and name='Planta +1';
+/*Esto fue parabuscar la planta 1, ya se que es 1 entonces ahora si programo la instrucción*/
+select name, floorid
+from spaces
+where floorid = 1;
 
 /* 10
 Nombre, número de modelo del tipo de componente con id = 60
 */
+select name, MODELNUMBER
+from component_types
+where id = 60;
 
 /* 11
 Nombre y fecha de instalación de los componentes del espacio 60 ordenados descendentemente por la fecha de instalación
 */
+select name, INSTALLATEDON
+from components
+where spaceid=60
+order by INSTALLATEDON desc;
+
 
 /* 12
 Listar las distintas fechas de instalación de los componentes del facility 1 ordenados descendentemente.
 */
+select distinct INSTALLATEDON
+from components
+where FACILITYID=1
+order by INSTALLATEDON desc;
 
 /* 13
 Listar los distintos GUIDs de los componentes del facility 1 ordenados ascendentemente por fecha de garantía.
