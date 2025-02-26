9ecc5b5cb35f6190157941dbae8d71e74440f54e
NATALIACABALLERO-IDESIE <natalia.caballero@idesie.com>
2025-02-26 18:39:12 +0100

    CORRECCIONES

--- 1 Select simples/Alegra/1.1_Selects-simples_G1B.sql
+++ 1 Select simples/Alegra/1.1_Selects-simples_G1B.sql
@@ -93,157 +93,158 @@ order by installatedon desc;
 /* 13
 Listar los distintos GUIDs de los componentes del facility 1 ordenados ascendentemente por fecha de garantía.
 */
-SELECT DISTINCT GUID
-FROM Componentes
-WHERE FacilityID = 1
-ORDER BY FechaGarantia ASC;
+select distinct externalidentifier
+from components
+where facilityid = 1
+order  by warrantystarton asc;
 /* 14
 Id, código de activo, GUID, número de serie y nombre de los componentes cuyo spaceid está entre 10 y 27 inclusive
 ordenados por id de espacio descendentemente.
 */
-SELECT Id, CodigoActivo, GUID, NumeroSerie, Nombre
-FROM Componentes
-WHERE SpaceID BETWEEN 10 AND 27
-ORDER BY SpaceID DESC;
+select id, spaceid, assetidentifier, externalidentifier, serialnumber
+from components
+where spaceid between 10 and 27
+order by spaceid desc;
 /* 15
 Id, código de activo, GUID, número de serie y nombre de los componentes del facility 1 
 ordenados por código de activo descendentemente.
 */
-SELECT Id, CodigoActivo, GUID, NumeroSerie, Nombre
-FROM Componentes
-WHERE FacilityID = 1
-ORDER BY CodigoActivo DESC;
+select id, assetidentifier, externalidentifier, serialnumber, name
+from components
+where facility = 1
+orderby warrantystarton desc;
 /* 16
 Códigos de activo de los componentes del espacio con id 21
 ordenados por código de activo descendentemente.
 */
-SELECT CodigoActivo
-FROM Componentes
-WHERE SpaceID = 21
-ORDER BY CodigoActivo DESC;
+select assetidentifier
+from components 
+where spaceid = 21
+order by assetidentifier desc;
 /* 17
 Las distintas fechas de instalación de los componentes 
 de los espacios con id 10, 12, 16, 19 
 ordenadas descendentemente.
 */
-SELECT DISTINCT FechaInstalacion
-FROM Componentes
-WHERE SpaceID IN (10, 12, 16, 19)
-ORDER BY FechaInstalacion DESC;
+select distinct installedon
+from components
+where spaceid in (10, 12, 16, 19)
+order by installedon desc;
 /* 18
 Nombre, volumen, de los espacios
 cuyo volumen es mayor a 90 de floorid = 1
 ordenados por volumen descendentemente
 */
-SELECT Nombre, Volumen
-FROM Espacios
-WHERE Volumen > 90 AND FloorID = 1
-ORDER BY Volumen DESC;
+select name, volume
+from spaces
+where volume >90 and floorid = 1
+order by volume desc;
 /* 19
 Nombre, volumen de los espacios
 cuyo volumen es mayor a 6 y menor a 9 de la planta con id = 1
 */
-SELECT Nombre, Volumen
-FROM Espacios
-WHERE Volumen > 6 AND Volumen < 9 AND FloorID = 1;
+select name, volume
+from spaces
+where volume >6 and volume < 9 and floorid = 1;
 /* 20
 Nombre, código de activo, número de serie de los componentes
 que no tengan espacio del facility 1
 ordenados descendentemente por código de activo
 */
-SELECT Nombre, CodigoActivo, NumeroSerie
-FROM Componentes
-WHERE SpaceID IS NULL AND FacilityID = 1
-ORDER BY CodigoActivo DESC;
+select name, assetidentifier, serialnumber
+from components
+where spaceid is null and facilityid = 1
+order by assetidentifier desc;
 /* 21
 Nombre, código de activo, número de serie de los componentes
 que tengan número de serie del facility 1
 */
-SELECT Nombre, CodigoActivo, NumeroSerie
-FROM Componentes
-WHERE NumeroSerie IS NOT NULL AND FacilityID = 1;
+select name, assetidentifier, serialnumber
+from components
+where serialnumber is not null and facilityid = 1; 
 /* 22
 Nombre de los espacios que empiezan por la letra A donde floorid = 1
 */
-SELECT Nombre
-FROM Espacios
-WHERE Nombre LIKE 'A%' AND FloorID = 1;
+select name
+from spaces
+Where name like 'A%' and floorid = 1;
 /* 23
 Lista de espacios que su segunda letra es una 's' donde floorid = 1
 */
-SELECT Nombre
-FROM Espacios
-WHERE Nombre LIKE '_s%' AND FloorID = 1;
+select name
+from spaces
+Where name like '_s%' and floorid = 1; 
 /* 24
 Lista de tipos de componente del facility 1 
 donde el nombre contiene el texto 'con'
 y no tienen vida útil indicada o fecha de garantia 
 */
-SELECT DISTINCT TipoComponente
-FROM Componentes
-WHERE FacilityID = 1
-     AND Nombre LIKE '%con%'
-     AND (VidaUtil IS NULL OR FechaGarantia IS NULL);
+select distinct name 
+from component_types
+where facilityid = 1
+     and name like '%con%'
+     AND (expectedlife is null or warrantystarton is null));
 /* 25
 Nombres de espacios y volumen
 pero como volumen una etiqueta que indique 
 'BAJO' si es menor a 10, 'ALTO' si es mayor a 1000
 y 'MEDIO' si está entre medias
 */
-SELECT Nombre,
-       CASE 
-           WHEN Volumen < 10 THEN 'BAJO'
-           WHEN Volumen > 1000 THEN 'ALTO'
-           ELSE 'MEDIO'
-       END AS VolumenEtiqueta
-FROM Espacios;
+select name,
+      case
+           when volume < 10 then 'low'
+           when volume > 1000 then 'high'
+           else 'medium'
+       end as volume_label
+from spaces;
 /* 26
 Nombre, fecha de instalación, fecha de garantia
 de los componentes del facility 1
 que tienen fecha de garantia
 */
-SELECT Nombre, FechaInstalacion, FechaGarantia
-FROM Componentes
-WHERE FacilityID = 1
-  AND FechaGarantia IS NOT NULL;
+select name, installedon, warrantystarton 
+from components 
+where facilityid = 1
+  and warrantystarton is not null;
 /* 27
 Lista de nombres de espacio que su id no es 4, 9, ni 19
 del floorid 1
 */
-SELECT Nombre
-FROM Espacios
-WHERE FloorID = 1
-  AND SpaceID NOT IN (4, 9, 19);
+select name
+from spaces
+where floorid = 1
+  and id not in (4, 9, 19);
 /* 28
 Lista de espacios que no son Aula del floorid = 1
 */
-SELECT Nombre
-FROM Espacios
-WHERE FloorID = 1
-  AND Nombre != 'Aula';
+select name 
+from spaces  
+where floorid = 1
+  and name != 'Aula';
 /* 29
 Lista de los tipos de componentes que tienen duracion de la garantia de las partes
 del facility 1
 */
-SELECT DISTINCT TipoComponente
-FROM Componentes
-WHERE FacilityID = 1
-  AND DuracionGarantia IS NOT NULL;
+select distinct name
+from component_types
+where facilityid = 1 
+and warrantyperiod is not null; 
+
 /* 30
 Lista de los tipos de componentes que no tiene el coste de repuesto
 del facility 1
 */
-SELECT DISTINCT TipoComponente
-FROM Componentes
-WHERE FacilityID = 1
-  AND CosteRepuesto IS NULL;
+select distinct name
+from component_types 
+where facilityid = 1 
+  and replacementcost is null;
 /* 31
 Lista de los tipos de componentes que tienen en el nombre un guión bajo
 del facility 1
 */
-SELECT DISTINCT TipoComponente
-FROM Componentes
-WHERE FacilityID = 1
-  AND Nombre LIKE '%\_%' ESCAPE '\';
+select distinct name
+from component_types 
+where faciityid = 1
+  and name like '%\_%' escape '\';
 --
 ------------------------------------------------------------------------------------------------
