472e2744ca3833240380bfd0cb4575b3d1b134aa
NATALIACABALLERO-IDESIE <natalia.caballero@idesie.com>
2025-03-31 17:49:11 +0200

    2.1 COMPLETO

--- 2 JOINS/Alegra/2.1_subconsultas y JOINS_GrupoG1B.sql
+++ 2 JOINS/Alegra/2.1_subconsultas y JOINS_GrupoG1B.sql
@@ -5,7 +5,15 @@
 Listar de la tabla facilities el id y nombre, 
 además de la tabla floors el id, nombre y facilityid
 */
-
+select
+    facilities.id,
+    facilities.name,
+    floors.id,
+    floors.name,
+    floors.facility_id
+from
+    facilities
+    join floors on facilities.id = floors.facility_id;
 /*2
 Lista de id de espacios que no están en la tabla de componentes (spaceid)
 pero sí están en la tabla de espacios.
@@ -154,7 +162,15 @@ group by
 ¿Cuál es la suma de áreas de los espacios por cada facility?
 Mostrar nombre del facility y la suma de las áreas 
 */
-
+select
+    facilities.name,
+    sum(spaces.area)
+from
+    facilities
+    join floors on facilities.id = floors.facility_id
+    join spaces on floors.id = spaces.floor_id
+group by
+    facilities.name;
 
 /*13
 ¿Cuántas sillas hay de cada tipo?
@@ -199,40 +215,131 @@ de todos los componentes
 del facility 1
 que estén en un aula y no sean tuberias, muros, techos, suelos.
 */
-
+select
+    components.name,
+    components.asset_code,
+    components.serial_number,
+    extract(year from components.installation_date),
+    spaces.name
+from
+    facilities
+    join floors on facilities.id = floors.facility_id
+    join spaces on floors.id = spaces.floor_id
+    join components on spaces.id = components.space_id
+where
+    facilities.id = 1
+    and spaces.name like 'Aula%'
+    and components.name not in ('Tubería', 'Muro', 'Techo', 'Suelo');
 
 /*
 15
 Nombre, área bruta y volumen de los espacios con mayor área que la media de áreas del facility 1.
 */
-
+select
+    spaces.name,
+    spaces.gross_area,
+    spaces.volume
+from
+    spaces
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+    and spaces.gross_area > (
+        select
+            avg(spaces.gross_area)
+        from
+            spaces
+            join floors on spaces.floor_id = floors.id
+            join facilities on floors.facility_id = facilities.id
+        where
+            facilities.id = 1
+    );
 
 /*
 16
 Nombre y fecha de instalación (yyyy-mm-dd) de los componentes del espacio con mayor área del facility 1
 */
-
+select
+    components.name,
+    to_char(components.installation_date, 'YYYY-MM-DD')
+from
+    components
+    join spaces on components.space_id = spaces.id
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+    and spaces.id = (
+        select
+            spaces.id
+        from
+            spaces
+            join floors on spaces.floor_id = floors.id
+            join facilities on floors.facility_id = facilities.id
+        where
+            facilities.id = 1
+        order by
+            spaces.area desc
+        limit 1
+    );
 
 /*
 17
 Nombre y código de activo  de los componentes cuyo tipo de componente contenga la palabra 'mesa'
 del facility 1
 */
-
+select
+    components.name,
+    components.asset_code
+from
+    components
+    join component_types on components.typeid = component_types.id
+    join spaces on components.space_id = spaces.id
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+    and component_types.name like '%mesa%';
 
 /*
 18
 Nombre del componente, espacio y planta de los componentes
 de los espacios que sean Aula del facility 1
 */
-
+select
+    components.name,
+    spaces.name,
+    floors.name
+from
+    components
+    join spaces on components.space_id = spaces.id
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+    and spaces.name like 'Aula%';
 
 /*
 19
 Número de componentes y número de espacios por planta (nombre) del facility 1. 
 Todas las plantas.
 */
-
+select
+    floors.name,
+    count(distinct spaces.id),
+    count(components.id)
+from
+    floors
+    join facilities on floors.facility_id = facilities.id
+    left join spaces on floors.id = spaces.floor_id
+    left join components on spaces.id = components.space_id
+where
+    facilities.id = 1
+group by
+    floors.name
+order by
+    floors.name;
 
 /*
 20
@@ -247,7 +354,25 @@ Componentes    Tipo   Espacio
 1   Mesa-profesor           Aula 3
 21  Mesa-cristal-redonda    Aula 12
 */
-
+select
+    count(components.id),
+    component_types.name,
+    spaces.name
+from
+    components
+    join component_types on components.typeid = component_types.id
+    join spaces on components.space_id = spaces.id
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+    and component_types.name like '%mesa%'
+group by
+    component_types.name,
+    spaces.name
+order by
+    spaces.name asc,
+    count(components.id) desc;
 
 /*
 21
@@ -264,7 +389,27 @@ Aula 1  BAJO
 Aula 2  BAJO
 Aula 3  MEDIO
 */
-
+select
+    spaces.name,
+    case
+        when count(components.id) < 6 then 'BAJO'
+        when count(components.id) > 15 then 'ALTO'
+        else 'MEDIO'
+    end
+from
+    components
+    join component_types on components.typeid = component_types.id
+    join spaces on components.space_id = spaces.id
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+    and spaces.name like 'Aula%'
+    and component_types.name like 'Silla%'
+group by
+    spaces.name
+order by
+    spaces.name;
 
 
 /*
@@ -279,20 +424,56 @@ Aula    18
 Aseo    4
 Hall    2
 */
-
+select
+    substr(spaces.name, 1, 4),
+    count(spaces.id)
+from
+    spaces
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+group by
+    substr(spaces.name, 1, 4)
+having
+    count(spaces.id) > 1
+order by
+    count(spaces.id) desc;
 
 /*
 23
 Nombre y área del espacio que mayor área bruta tiene del facility 1.
 */
-
+select
+    spaces.name,
+    spaces.area
+from
+    spaces
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+order by
+    spaces.area desc
+limit 1
 
 /*
 24
 Número de componentes instalados entre el 1 de mayo de 2010 y 31 de agosto de 2010
 y que sean grifos, lavabos del facility 1
 */
-
+select
+    count(components.id)
+from
+    components
+    join component_types on components.typeid = component_types.id
+    join spaces on components.space_id = spaces.id
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+    and components.installation_date between '2010-05-01' and '2010-08-31'
+    and component_types.name in ('Grifo', 'Lavabo');
 
 /*
 25
@@ -306,29 +487,127 @@ Componentes 70
 Sillas 16
 Mesas 3
 */
+select
+    'Componentes',
+    count(components.id)
+from
+    components
+    join spaces on components.space_id = spaces.id
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+    and spaces.name = 'Aula 03'
 
+union
+
+select
+    'Sillas',
+    count(components.id)
+from
+    components
+    join component_types on components.typeid = component_types.id
+    join spaces on components.space_id = spaces.id
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+    and spaces.name = 'Aula 03'
+    and component_types.name like 'Silla%'
+
+union
+
+select
+    'Mesas',
+    count(components.id)
+from
+    components
+    join component_types on components.typeid = component_types.id
+    join spaces on components.space_id = spaces.id
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+    and spaces.name = 'Aula 03'
+    and (
+        component_types.name like '%Mesa%'
+        or component_types.name like '%Escritorio%'
+    );
 
 /*
 26
 Nombre del espacio, y número de grifos del espacio con más grifos del facility 1.
 */
-
+select
+    spaces.name,
+    count(components.id)
+from
+    components
+    join component_types on components.typeid = component_types.id
+    join spaces on components.space_id = spaces.id
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+    and component_types.name = 'Grifo'
+group by
+    spaces.name
+order by
+    count(components.id) desc
+limit 1;
 
 /*
 27
 Cuál es el mes en el que más componentes se instalaron del facility 1.
 */
-
+select
+    extract(month from components.installation_date),
+    count(components.id)
+from
+    components
+    join spaces on components.space_id = spaces.id
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+group by
+    extract(month from components.installation_date)
+order by
+    count(components.id) desc
+limit 1;
 
 /* 28
 Nombre del día en el que más componentes se instalaron del facility 1.
 Ejemplo: Jueves
 */
-
+select
+    to_char(components.installation_date, 'Day'),
+    count(components.id)
+from
+    components
+    join spaces on components.space_id = spaces.id
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+group by
+    to_char(components.installation_date, 'Day')
+order by
+    count(components.id) desc
+limit 1;
 /*29
 Listar los nombres de componentes que están fuera de garantía del facility 1.
 */
-
+select
+    components.name
+from
+    components
+    join spaces on components.space_id = spaces.id
+    join floors on spaces.floor_id = floors.id
+    join facilities on floors.facility_id = facilities.id
+where
+    facilities.id = 1
+    and components.warranty_end_date < current_date;
 /*
 30
 Listar el nombre de los tres espacios con mayor área del facility 1
