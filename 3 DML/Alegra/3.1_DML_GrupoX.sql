d48351c0cb9a538c2119180dc6559006f1ddde15
Gerardole21 <gerardo.lopez@idesie.com>
2025-04-02 16:53:18 +0200

    Update 3.1_DML_GrupoX.sql

--- 3 DML/Alegra/3.1_DML_GrupoX.sql
+++ 3 DML/Alegra/3.1_DML_GrupoX.sql
@@ -14,6 +14,8 @@ id del espacio «7»
 id de tipo «78»
 guid «666000»
 */
+insert into components (name, description, serialnumber, installatedon, warrantystart, assetidentifier, creatorid, spaceid, typeid, guid)
+values ('Grifo | Grifo | 030303', 'test insert', '666333-eeefff', '2021-12-12', '2021-11-11', '666000', 3, 7, 78, '666000');
 
 /*
 Comprobar que se ven los datos insertados de forma conjunta con una JOIN
@@ -22,20 +24,33 @@ Mostrar todos los datos indicados en el punto anterior
 y además el nombre del espacio, nombre de la planta, nombre del tipo de componente
 */
 
+
 /* 2
 Eliminar el componente creado.
 */
+delete from components
+where guid = '666000';
 
 /* 3
 Colocar como código de barras los 6 últimos caracteres del GUID 
 a todo componente de la planta 1 y 2 del facility 1.
 */
+update components
+set barcode = right(guid, 6)
+where spaceid in (select id from spaces where floorid in (select id from floors where facilityid = 1 and id in (1, 2)));
 
 /* 4
 Modificar la fecha de garantia para que sea igual a la fecha de instalación
 para todo componente que sea un grifo o lavabo del facility 1.
 */
+update components
+set warrantystart = installatedon
+where spaceid in (select id from spaces where floorid in (select id from floors where facilityid = 1))
+and (name like '%grifo%' or name like '%lavabo%');
 
 /* 5
 Anonimizar los datos personales: nombre, apellido, email, teléfono de los contactos
 */
+update contacts
+set first_name = 'Anonymous', last_name = 'Anonymous', email = 'anonymous@example.com', phone = '000000000'
+where first_name is not null and last_name is not null;
